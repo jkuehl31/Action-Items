d6615b1055cc7ef607ce4fc84947814b03cc8380
John Kuehl <john.kuehl@jjindustries.com>
2018-07-16 19:49:47 -0700

    Action Items Complete with datasource and delegate methods.

---
+++ Action Items/ActionItemViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Action Items
+//
+//  Created by John Kuehl on 7/16/18.
+//  Copyright © 2018 Winners Win. All rights reserved.
+//
+
+import UIKit
+
+class ActionItemViewController: UITableViewController {
+    
+    let itemArray = ["Read", "Go to the Gym", "Learn new skill"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    }
+
+//MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ActionItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+
+}
+
